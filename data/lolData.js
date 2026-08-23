26852bb2cf0ae880aa192cd3db050558229dc34c
SerXa8 <152960521+SerXa8@users.noreply.github.com>
2026-08-23 17:23:01 +0200

    Update lolData.js

--- data/lolData.js
+++ data/lolData.js
@@ -19,9 +19,21 @@ const gameData = {
       "lossEvents": 0,
       "spark": "M 0 15 L 7.5 18 L 15.0 15 L 22.5 12 L 30.0 9 L 37.5 12 L 45.0 15 L 52.5 12 L 60.0 9 L 67.5 6 L 75.0 3",
       "twitch": "javierrlol",
+      "isLive": false,
+      "viewers": 0,
       "inGame": false,
       "champion": null,
-      "rank": 1
+      "rank": 1,
+      "topMasteries": [
+        { "championName": "Ahri", "championLevel": 10, "championPoints": 350000 },
+        { "championName": "Syndra", "championLevel": 9, "championPoints": 280000 },
+        { "championName": "Orianna", "championLevel": 8, "championPoints": 210000 }
+      ],
+      "recentMatches": [
+        { "win": true, "championName": "Ahri", "kills": 10, "deaths": 2, "assists": 8, "cs": 240, "gameDurationMinutes": 26 },
+        { "win": true, "championName": "Syndra", "kills": 7, "deaths": 3, "assists": 5, "cs": 210, "gameDurationMinutes": 22 },
+        { "win": false, "championName": "Orianna", "kills": 3, "deaths": 5, "assists": 9, "cs": 280, "gameDurationMinutes": 34 }
+      ]
     },
     {
       "name": "Maiiser",
@@ -42,9 +54,20 @@ const gameData = {
       "lossEvents": 0,
       "spark": "M 0 15 L 7.5 12 L 15.0 9 L 22.5 12 L 30.0 9 L 37.5 6 L 45.0 3 L 52.5 2 L 60.0 2 L 67.5 2 L 75.0 5",
       "twitch": "maiiser",
+      "isLive": false,
+      "viewers": 0,
       "inGame": false,
       "champion": null,
-      "rank": 2
+      "rank": 2,
+      "topMasteries": [
+        { "championName": "Aatrox", "championLevel": 10, "championPoints": 420000 },
+        { "championName": "Renekton", "championLevel": 8, "championPoints": 190000 },
+        { "championName": "Jax", "championLevel": 7, "championPoints": 150000 }
+      ],
+      "recentMatches": [
+        { "win": true, "championName": "Aatrox", "kills": 9, "deaths": 1, "assists": 4, "cs": 205, "gameDurationMinutes": 24 },
+        { "win": false, "championName": "Renekton", "kills": 2, "deaths": 6, "assists": 2, "cs": 180, "gameDurationMinutes": 29 }
+      ]
     },
     {
       "name": "SerXa8",
@@ -65,9 +88,20 @@ const gameData = {
       "lossEvents": 0,
       "spark": "M 0 15 L 7.5 18 L 15.0 21 L 22.5 18 L 30.0 15 L 37.5 12 L 45.0 9 L 52.5 6 L 60.0 3 L 67.5 2 L 75.0 2",
       "twitch": "s3rxa8",
+      "isLive": false,
+      "viewers": 0,
       "inGame": false,
       "champion": null,
-      "rank": 3
+      "rank": 3,
+      "topMasteries": [
+        { "championName": "Garen", "championLevel": 7, "championPoints": 85000 },
+        { "championName": "Darius", "championLevel": 6, "championPoints": 62000 },
+        { "championName": "Mordekaiser", "championLevel": 5, "championPoints": 41000 }
+      ],
+      "recentMatches": [
+        { "win": false, "championName": "Garen", "kills": 4, "deaths": 7, "assists": 3, "cs": 150, "gameDurationMinutes": 30 },
+        { "win": true, "championName": "Darius", "kills": 12, "deaths": 4, "assists": 5, "cs": 190, "gameDurationMinutes": 27 }
+      ]
     },
     {
       "name": "mamielizabeth",
@@ -88,9 +122,19 @@ const gameData = {
       "lossEvents": 0,
       "spark": "M 0 15 L 7.5 18 L 15.0 15 L 22.5 12 L 30.0 15 L 37.5 12 L 45.0 15 L 52.5 12 L 60.0 9 L 67.5 6 L 75.0 3",
       "twitch": "",
+      "isLive": false,
+      "viewers": 0,
       "inGame": false,
       "champion": null,
-      "rank": 4
+      "rank": 4,
+      "topMasteries": [
+        { "championName": "Lux", "championLevel": 7, "championPoints": 110000 },
+        { "championName": "Veigar", "championLevel": 6, "championPoints": 73000 },
+        { "championName": "Neeko", "championLevel": 5, "championPoints": 35000 }
+      ],
+      "recentMatches": [
+        { "win": true, "championName": "Lux", "kills": 6, "deaths": 2, "assists": 14, "cs": 130, "gameDurationMinutes": 25 }
+      ]
     }
   ]
 };
